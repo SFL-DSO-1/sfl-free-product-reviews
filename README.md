3c848f785f6a6c557b5d853620dfa35191e17fc0
SFL-DSO-Orlando <orlando@smartforlife.com>
2022-07-29 11:43:02 -0400

    Added Powered by SMFL.

--- README.md
+++ README.md
@@ -18,6 +18,7 @@
 
 ## Release History ##
 
+* v3.3.1 - 07/29/2022 - Added Powered by SMFL.
 * v3.3.0 - 06/21/2022 - Separated US and Canada Product Pickers. Added BV Campaigns.
 * v3.2.11 - 06/14/2022 - Google Analytics 4 Install.
 * v3.2.10 - 05/24/2022 - Campaign IDs.
--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,5 +1,8 @@
 <footer class="container">
     <p>&copy; Smart for Life <span id="year"></span></p>
+    <div class="image-section" style="background-color:#4c6631;">       
+        <a href="https://smartforlifecorp.com/welcome" target="_blank"><img width="350" height="70" src="https://storage.googleapis.com/smartforlife/brand/smfl-powered-by350x70.png" alt="Powered by Smart for Life, Inc. Nasdaq: SMFL" title="Powered by Smart for Life, Inc. –&nbsp;Nasdaq: SMFL"></a>
+    </div>
 </footer>
 <script src="vendor/jquery-v3.4.1.min.js"></script>
 <script src="vendor/bootstrap-v4.0.0/bootstrap.min.js" crossorigin="anonymous"></script>
